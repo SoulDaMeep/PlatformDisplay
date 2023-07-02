9a12e196774a44356caee3dabb1db4ffda2bd0dc
it-like-book <121825658+it-like-book@users.noreply.github.com>
2023-07-02 10:43:23 -0400

    Switch to spaces for indentation.
    Sick of the inconsistent views between visual studio and github.

--- PlatformDisplay/PlatformDisplay.h
+++ PlatformDisplay/PlatformDisplay.h
@@ -10,86 +10,86 @@
 #include <string>
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
-class PlatformDisplay :	public BakkesMod::Plugin::BakkesModPlugin,
-	public BakkesMod::Plugin::PluginSettingsWindow,
-	public BakkesMod::Plugin::PluginWindow {
+class PlatformDisplay : public BakkesMod::Plugin::BakkesModPlugin,
+  public BakkesMod::Plugin::PluginSettingsWindow,
+  public BakkesMod::Plugin::PluginWindow {
 public:
-	struct Pri {
-		UniqueIDWrapper uid;
-		int score{};
-		unsigned char team{};
-		bool isBot{};
-		std::string name;
-		OnlinePlatform platform;
+  struct Pri {
+    UniqueIDWrapper uid;
+    int score{};
+    unsigned char team{};
+    bool isBot{};
+    std::string name;
+    OnlinePlatform platform;
 
-		Pri() {}
-		Pri(PriWrapper p) {
-			if (!p) { return; }
-			uid = p.GetUniqueIdWrapper();
-			score = p.GetMatchScore();
-			team = p.GetTeamNum2();
-			isBot = p.GetbBot();
-			name = p.GetPlayerName().ToString();
-			platform = p.GetPlatform();
-		}
-	};
+    Pri() {}
+    Pri(PriWrapper p) {
+      if (!p) { return; }
+      uid = p.GetUniqueIdWrapper();
+      score = p.GetMatchScore();
+      team = p.GetTeamNum2();
+      isBot = p.GetbBot();
+      name = p.GetPlayerName().ToString();
+      platform = p.GetPlatform();
+    }
+  };
 private:
-	/**
-	 * Stores data derived from each scoreboard sort cycle (happens once every second).
-	 */
-	struct ComputedScoreboardInfo {
-		std::vector<Pri>sortedPlayers;
-		int bluePlayerCount{};
-		int orangePlayerCount{};
-	};
+  /**
+   * Stores data derived from each scoreboard sort cycle (happens once every second).
+   */
+  struct ComputedScoreboardInfo {
+    std::vector<Pri>sortedPlayers;
+    int bluePlayerCount{};
+    int orangePlayerCount{};
+  };
 
-	virtual void onLoad();
-	/**
-	 * Pre-compute scoreboard info after the sorting algorithm finishes. The hook
-	 * "TAGame.PRI_TA.GetScoreboardStats" runs at least once immediately after Rocket League sorts
-	 * the scoreboard. This happens every second, so computing the info like this saves us some
-	 * performance.
-	 */
-	void ComputeScoreboardInfo();
-	void RecordScoreboardComparison(ActorWrapper gameEvent, void* params, std::string eventName);
-	void RenderPlatformLogos(CanvasWrapper canvas);
-	void RenderDebugInfo(CanvasWrapper canvas);
-	void RenderDebugPri(CanvasWrapper canvas, const PlatformDisplay::Pri& pri, Vector2F iconPos);
-	void SetTeamColors(bool keepOrder = false);
+  virtual void onLoad();
+  /**
+   * Pre-compute scoreboard info after the sorting algorithm finishes. The hook
+   * "TAGame.PRI_TA.GetScoreboardStats" runs at least once immediately after Rocket League sorts
+   * the scoreboard. This happens every second, so computing the info like this saves us some
+   * performance.
+   */
+  void ComputeScoreboardInfo();
+  void RecordScoreboardComparison(ActorWrapper gameEvent, void* params, std::string eventName);
+  void RenderPlatformLogos(CanvasWrapper canvas);
+  void RenderDebugInfo(CanvasWrapper canvas);
+  void RenderDebugPri(CanvasWrapper canvas, const PlatformDisplay::Pri& pri, Vector2F iconPos);
+  void SetTeamColors(bool keepOrder = false);
 
-	void RenderSettings() override;
-	std::string GetPluginName() override;
-	virtual void Render() override;
-	virtual std::string GetMenuName() override;
-	virtual std::string GetMenuTitle() override;
-	virtual void SetImGuiContext(uintptr_t ctx) override;
-	virtual bool ShouldBlockInput() override;
-	virtual bool IsActiveOverlay() override;
-	virtual void OnOpen() override;
-	virtual void OnClose() override;
+  void RenderSettings() override;
+  std::string GetPluginName() override;
+  virtual void Render() override;
+  virtual std::string GetMenuName() override;
+  virtual std::string GetMenuTitle() override;
+  virtual void SetImGuiContext(uintptr_t ctx) override;
+  virtual bool ShouldBlockInput() override;
+  virtual bool IsActiveOverlay() override;
+  virtual void OnOpen() override;
+  virtual void OnClose() override;
 
-	// Members for scoreboard tracking logic.
-	std::vector<std::pair<Pri, Pri>> comparisons;
-	/**
-	 * teamHistory records the last team that a player (represented by the 
-	 * string combining their name and uid) was seen on. This is necessary, 
-	 * because during ScoreboardSort any disconnected players will have a team 
-	 * number different from Blue or Orange, but we still need to know which team
-	 * they show up on in the scoreboard display.
-	 */
-	std::unordered_map<std::string, int> teamHistory;
-	ComputedScoreboardInfo computedInfo{};  // Derived from comparisons and teamHistory.
-	bool accumulateComparisons{};
+  // Members for scoreboard tracking logic.
+  std::vector<std::pair<Pri, Pri>> comparisons;
+  /**
+   * teamHistory records the last team that a player (represented by the
+   * string combining their name and uid) was seen on. This is necessary,
+   * because during ScoreboardSort any disconnected players will have a team
+   * number different from Blue or Orange, but we still need to know which team
+   * they show up on in the scoreboard display.
+   */
+  std::unordered_map<std::string, int> teamHistory;
+  ComputedScoreboardInfo computedInfo{};  // Derived from comparisons and teamHistory.
+  bool accumulateComparisons{};
 
-	// Members for scoreboard rendering.
-	bool scoreBoardOpen{};
-	LinearColor teamColors[2]{ {255, 255, 255, 255}, {255, 255, 255, 255} };
-	const static int LOGO_COUNT = 6;
-	std::shared_ptr<ImageWrapper> logos[LOGO_COUNT];
-	std::shared_ptr<ImageWrapper> noTintLogos[LOGO_COUNT];
+  // Members for scoreboard rendering.
+  bool scoreBoardOpen{};
+  LinearColor teamColors[2]{ {255, 255, 255, 255}, {255, 255, 255, 255} };
+  const static int LOGO_COUNT = 6;
+  std::shared_ptr<ImageWrapper> logos[LOGO_COUNT];
+  std::shared_ptr<ImageWrapper> noTintLogos[LOGO_COUNT];
 
-	// Members for menus.
-	bool windowOpen{};
-	std::string menuTitle{ "PlatformDisplay" };
+  // Members for menus.
+  bool windowOpen{};
+  std::string menuTitle{ "PlatformDisplay" };
 };
 
--- PlatformDisplay/ScoreboardPositionInfo.h
+++ PlatformDisplay/ScoreboardPositionInfo.h
@@ -18,44 +18,44 @@
 
 // Scoreboard Position Info: 
 struct SbPosInfo {
-	Vector2F blueLeaderPos;  // position of the profile box for the top blue player
-	Vector2F orangeLeaderPos;
-	float playerSeparation;  // distance between profile pics on each team
-	float profileScale;      // amount to scale each platform icon / profile pic
+  Vector2F blueLeaderPos;  // position of the profile box for the top blue player
+  Vector2F orangeLeaderPos;
+  float playerSeparation;  // distance between profile pics on each team
+  float profileScale;      // amount to scale each platform icon / profile pic
 };
 
 SbPosInfo getSbPosInfo(Vector2F canvasSize, float uiScale, bool mutators, int numBlues, int numOranges) {
-	//-----Black Magic, thanks BenTheDan------------
-	float scale;
-	if (canvasSize.X / canvasSize.Y > 1.5f) {
-		scale = 0.507f * canvasSize.Y / SCOREBOARD_HEIGHT;
-	}
-	else {
-		scale = 0.615f * canvasSize.X / SCOREBOARD_WIDTH;
-	}
+  //-----Black Magic, thanks BenTheDan------------
+  float scale;
+  if (canvasSize.X / canvasSize.Y > 1.5f) {
+    scale = 0.507f * canvasSize.Y / SCOREBOARD_HEIGHT;
+  }
+  else {
+    scale = 0.615f * canvasSize.X / SCOREBOARD_WIDTH;
+  }
 
-	Vector2F center = Vector2F{ canvasSize.X / 2, canvasSize.Y / 2 };
-	float mutators_center = canvasSize.X - 1005.0f * scale * uiScale;
-	if (mutators && mutators_center < center.X) {
-		center.X = mutators_center;
-	}
-	int team_difference = numBlues - numOranges;
-	center.Y += IMBALANCE_SHIFT * (team_difference - ((numBlues == 0) != (numOranges == 0)) * (team_difference >= 0 ? 1 : -1)) * scale * uiScale;
+  Vector2F center = Vector2F{ canvasSize.X / 2, canvasSize.Y / 2 };
+  float mutators_center = canvasSize.X - 1005.0f * scale * uiScale;
+  if (mutators && mutators_center < center.X) {
+    center.X = mutators_center;
+  }
+  int team_difference = numBlues - numOranges;
+  center.Y += IMBALANCE_SHIFT * (team_difference - ((numBlues == 0) != (numOranges == 0)) * (team_difference >= 0 ? 1 : -1)) * scale * uiScale;
 
-	SbPosInfo output;
-	output.profileScale = 0.48f;
-	float tier_X = -SCOREBOARD_LEFT - (IMAGE_WIDTH - 100.0f) * output.profileScale + 30.5f;
-	tier_X = center.X + tier_X * scale * uiScale;
-	float tier_Y_blue = -BLUE_BOTTOM + (6 * (4 - numBlues)) - BANNER_DISTANCE * numBlues + 9.0f;
-	tier_Y_blue = center.Y + tier_Y_blue * scale * uiScale;
-	float tier_Y_orange = ORANGE_TOP;
-	tier_Y_orange = center.Y + tier_Y_orange * scale * uiScale;
-	output.blueLeaderPos = { tier_X, tier_Y_blue };
-	output.orangeLeaderPos = { tier_X, tier_Y_orange };
-	output.playerSeparation = BANNER_DISTANCE * scale * uiScale;
-	output.profileScale *= scale * uiScale;
+  SbPosInfo output;
+  output.profileScale = 0.48f;
+  float tier_X = -SCOREBOARD_LEFT - (IMAGE_WIDTH - 100.0f) * output.profileScale + 30.5f;
+  tier_X = center.X + tier_X * scale * uiScale;
+  float tier_Y_blue = -BLUE_BOTTOM + (6 * (4 - numBlues)) - BANNER_DISTANCE * numBlues + 9.0f;
+  tier_Y_blue = center.Y + tier_Y_blue * scale * uiScale;
+  float tier_Y_orange = ORANGE_TOP;
+  tier_Y_orange = center.Y + tier_Y_orange * scale * uiScale;
+  output.blueLeaderPos = { tier_X, tier_Y_blue };
+  output.orangeLeaderPos = { tier_X, tier_Y_orange };
+  output.playerSeparation = BANNER_DISTANCE * scale * uiScale;
+  output.profileScale *= scale * uiScale;
 
-	// based on 100x100 img
-	//------End Black Magic---------
-	return output;
+  // based on 100x100 img
+  //------End Black Magic---------
+  return output;
 }
